b65b96724b6adf17a5083f5622eb2a0b604d0260
Hong Ngoc Nguyen <30820288+nguyenhongngoc09@users.noreply.github.com>
2024-01-02 12:41:56 +0000

    feat: Add Swagger documentation
    Added Swagger documentation using @nestjs/swagger to provide API documentation.

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,22 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Example')
+    .setDescription('NestJS Example API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,17 @@
-import { Controller, Get, Param, ParseIntPipe } from "@nestjs/common";
-import { UsersService } from "./users.service";
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { UsersService } from './users.service';
 
-@Controller("/users")
+@Controller('/users')
 export class UsersController {
-  constructor(
-    private readonly usersService: UsersService
-  ) {
-  }
+  constructor(private readonly usersService: UsersService) {}
 
   @Get()
   async getAllUsers(): Promise<any> {
     return await this.usersService.getUsers();
   }
 
-  @Get(":userId/posts")
-  getUserPosts(@Param("userId", ParseIntPipe) userId: number): any {
+  @Get(':userId/posts')
+  getUserPosts(@Param('userId', ParseIntPipe) userId: number): any {
     return this.usersService.getUserPosts(userId);
   }
 }
